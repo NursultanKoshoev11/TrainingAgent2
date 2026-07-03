e3d9ee20f80435539d9c5d8516c17ce7118ab76b
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-07-03 12:05:38 +0600

    Include explanation API routes

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 from app.api import router
+from app.api_explain import explain_router
 from app.api_extra import extra_router
 
 server = FastAPI()
 server.include_router(router)
 server.include_router(extra_router)
+server.include_router(explain_router)
