f99e22bb41d6ebfc0ed1d702d9dd45f030ec954d
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-07-03 11:58:18 +0600

    Include extra API routes

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from app.api import router
+from app.api_extra import extra_router
 
 server = FastAPI()
 server.include_router(router)
+server.include_router(extra_router)
