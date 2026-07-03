c56b5cd788409f15e241230b0d5f605dfc786e91
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-07-03 12:14:17 +0600

    Include screener API routes

--- backend/app/server.py
+++ backend/app/server.py
@@ -3,8 +3,10 @@ from fastapi import FastAPI
 from app.api import router
 from app.api_explain import explain_router
 from app.api_extra import extra_router
+from app.api_screener import screener_router
 
 server = FastAPI()
 server.include_router(router)
 server.include_router(extra_router)
 server.include_router(explain_router)
+server.include_router(screener_router)
